db25b0c09e50fbe1ec697227fca46e0a0d866dd2
caseyanderson <cta@caseyanderson.com>
2019-02-01 15:44:10 -0800

    ignore ipython checkpoints and np files;

--- button_led_1x1.py
+++ button_led_1x1.py
@@ -6,7 +6,7 @@ import machine
 import time
 
 button = machine.Pin(12, machine.Pin.IN, machine.Pin.PULL_UP)
-led = machine.Pin(13, machine.Pin.OUT)
+led = machine.Pin(27, machine.Pin.OUT)
 
 while True:
     if not button.value():
