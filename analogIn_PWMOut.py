3c03ac24d40b11a08d330d69970082e3c82147dd
caseyanderson <cta@caseyanderson.com>
2019-02-06 14:52:57 -0800

    update pin number for led, condense PWM instantiation to one line

--- analogIn_PWMOut.py
+++ analogIn_PWMOut.py
@@ -3,19 +3,17 @@
 from machine import ADC, Pin, PWM
 from time import sleep_ms
 
-ledPin = Pin(15)
+ledPin = Pin(27)
 potPin = Pin(34)
 
 pot = ADC(potPin)
 pot.atten(ADC.ATTN_11DB)
 pot.width(ADC.WIDTH_10BIT) # 0 - 1023
 
-pwm = PWM(ledPin)
-pwm.freq(100)
+pwm = PWM(Pin(27), freq=20000, duty=0)
 
 while True:
     sensor_val = pot.read()
     print(sensor_val)
     pwm.duty(sensor_val)
-
     sleep_ms(20)
