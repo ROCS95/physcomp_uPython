43a582f2ef33fdd5f00c1b1cbc35faff03a03f67
caseyanderson <cta@caseyanderson.com>
2019-02-06 11:49:12 -0800

    previous fade.py is now breathe.py

--- breathe.py
+++ breathe.py
@@ -1,18 +1,23 @@
-''' fade.py
+'''
+    breathe.py
 
     sourced from https://learn.adafruit.com/micropython-hardware-analog-i-o/pulse-width-modulation
 '''
 
 from time import sleep
-from machine Pin, PWM
+from machine import Pin, PWM
 
-pwm = PWM(Pin(15))
+pwm = PWM(Pin(27))
 pwm.freq(60)
 
 while True:
     for i in range(1024):
+        if i == 0:
+            print("inhale")
         pwm.duty(i)
         sleep(0.001)
     for i in range(1023, -1, -1):
+        if i == 1023:
+            print("exhale")
         pwm.duty(i)
         sleep(0.001)
