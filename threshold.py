a47c8b5a694c058409ea355f5eda495c5a0ecc05
caseyanderson <cta@caseyanderson.com>
2019-02-06 14:54:30 -0800

    laskdf

--- threshold.py
+++ threshold.py
@@ -1,20 +1,22 @@
-# threshold.py
+'''
+threshold.py
+'''
 
-import machine
+from machine import ADC, Pin
 from time import sleep_ms
 
-potPin = machine.Pin(34, machine.Pin.IN)
+potPin = Pin(34, Pin.IN)
+led = Pin(27, Pin.OUT)
 
-pot = machine.ADC(potPin)
-pot.atten(machine.ADC.ATTN_11DB)
-pot.width(machine.ADC.WIDTH_10BIT) # 0 - 1023
-
-led = machine.Pin(15, machine.Pin.OUT)
+pot = ADC(potPin)
+pot.atten(ADC.ATTN_11DB)
+pot.width(ADC.WIDTH_10BIT) # 0 - 1023
 
 while True:
-    sensor_val = pot.read()
-    if sensor_val >= 512:
-        led.value(1)
-    else:
-        led.value(0)
-    sleep_ms(20)
+  sensor_val = pot.read()
+  if sensor_val >= 512:
+    print("crossed threshold!")
+    led.value(1)
+  else:
+    led.value(0)
+  sleep_ms(20)
