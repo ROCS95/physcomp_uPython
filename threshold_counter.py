0a668f0a59234d13e079deeda29491781510c16b
caseyanderson <cta@caseyanderson.com>
2019-03-11 15:59:05 -0700

    this example is done now

--- threshold_counter.py
+++ threshold_counter.py
@@ -11,22 +11,28 @@ pot = ADC(potPin)
 pot.atten(ADC.ATTN_11DB)
 pot.width(ADC.WIDTH_10BIT) # 0 - 1023
 
+threshold = 200
+
 prev_val = pot.read()
 counter = 0
 limit = 5
 
 while True:
     current_val = pot.read()
-    if current_val - prev_val >= 200:
-        print("press detected!")
+    # val = ' '.join(["current val is", str(current_val)])
+    # print(val)
+    diff = current_val - prev_val
+    if diff >= threshold:
+        press = ' '.join(["diff is", str(diff), "trigger detected!" ])
+        print(press)
         counter+=1
         current_count = ' '.join(["counter is", str(counter)])
         print(current_count)
         sleep_ms(500) # this is a hack but stops repeated counts
         if counter == limit:
             print("DONE!")
-            break # uncomment to break out of loop
-            # counter = 0 # uncomment to reset loop to 0
+            # break # uncomment to break out of loop
+            counter = 0 # uncomment to reset loop to 0
     else:
         diff = current_val - prev_val
         # print(diff)
